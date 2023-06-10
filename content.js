c446525dad43f6235d99775a98e9891c373e97f4
Yen Chen <wai.imyen@gmail.com>
2023-06-11 03:20:09 +0800

    add: make ad blocker

---
+++ content.js
@@ -0,0 +1,6 @@
+// Remove ads
+var adElement = document.getElementById("viptoneWindow");
+if(adElement) {
+  adElement.remove();
+}
+console.log('test.test.test.test.test.test.test.test.test.test.test.test.test.test.test.test.test.test.test.');
